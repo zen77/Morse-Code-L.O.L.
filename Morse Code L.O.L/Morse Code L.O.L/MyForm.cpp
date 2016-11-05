78c549f57f270c851454ffee0d973db23b0b4af5
zen77 <tmaloney2@owlmail.harford.edu>
2016-11-04 23:38:54 -0400

    Added Release

--- Morse Code L.O.L/Morse Code L.O.L/MyForm.cpp
+++ Morse Code L.O.L/Morse Code L.O.L/MyForm.cpp
@@ -3,10 +3,14 @@ GitHub: https://github.com/zen77/Morse-Code-L.O.L.
 Copyright © 2016 T Malo / Zen77
 */
 #include "MyForm.h"
+#include <Windows.h>
 
 using namespace System;
 using namespace System::Windows::Forms;
 
+//+------------------------------+
+//Closes console window if true
+//Console window is open if false
 
 [STAThread]
 void Main(array<String^>^ args)
@@ -14,6 +18,8 @@ void Main(array<String^>^ args)
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 
+
 	Morse_Code_L_O_L::MyForm form;
 	Application::Run(%form);
+
 }
